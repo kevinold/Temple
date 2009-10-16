28dc3fc8e28a7891d960216c248d1f8593268f4e
Kevin Old <kevin@kevinold.com>
2009-10-16 18:30:17 -0500

    new design with slices!!!

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,22 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+/* $(function() {
+
+    // Slide ministry index - up or down
+    // from stonebriar.org
+    $('#ministry_tab').toggle(function() {
+        $('#ministry_index:hidden').slideDown();
+        $('#ministry_tab')[0].blur();
+        $(this).addClass('on');
+        return false;
+    },
+    function() {
+        $('#ministry_index:visible').slideUp();
+        $('#ministry_tab')[0].blur();
+        $(this).removeClass('on');
+        return false;
+    }); 
+
+}); */
+
