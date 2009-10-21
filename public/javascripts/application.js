1ef5d223750f138078ae638ebb34bd8cfc88c501
Kevin Old <kevin@kevinold.com>
2009-10-20 23:28:33 -0500

    code sliding services & info tab to header

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,22 +1,16 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-/* $(function() {
-
-    // Slide ministry index - up or down
-    // from stonebriar.org
-    $('#ministry_tab').toggle(function() {
-        $('#ministry_index:hidden').slideDown();
-        $('#ministry_tab')[0].blur();
-        $(this).addClass('on');
-        return false;
-    },
-    function() {
-        $('#ministry_index:visible').slideUp();
-        $('#ministry_tab')[0].blur();
-        $(this).removeClass('on');
-        return false;
-    }); 
-
-}); */
+$(function() {
+  // Toggles the services overlay for the header
+  $("a#services-tab").toggle(
+      function () {
+        $("#services-overlay").animate({top: "0px"}, 400);
+        $(".down").addClass("up");
+      },
+      function () {
+        $("#services-overlay").animate({top: "-175px"}, 400);
+        $(".down").removeClass("up");
+  });
+});
 
