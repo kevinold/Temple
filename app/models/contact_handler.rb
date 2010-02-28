52de65feb08ab7eeb68cab0b6833930c29dfcd52
Kevin Old <kevin@kevinold.com>
2010-02-28 16:50:45 -0600

    get general contact form working

--- app/models/contact_handler.rb
+++ app/models/contact_handler.rb
@@ -1,8 +1,8 @@
 class ContactHandler < ActiveForm
 
-  validates_presence_of :subject, :body
+  validates_presence_of :name, :email, :subject, :body
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
   
-  attr_accessor :subject, :email, :body
+  attr_accessor :name, :subject, :email, :body
 
 end
--- app/models/contact_mailer.rb
+++ app/models/contact_mailer.rb
@@ -2,9 +2,10 @@ class ContactMailer < ActionMailer::Base
   
   def contact_notification(contact_handler)
     
-    @recipients  = "kevin@kevinold.com"
+    @recipients  = "tech@tbwh.org"
     @from        = contact_handler.email
-    @subject     = "Contact form: #{contact_handler.subject}"
+    @name        = contact_handler.name
+    @subject     = "[Contact form] #{contact_handler.subject}"
     
     @body[:contact_handler] = contact_handler
     
