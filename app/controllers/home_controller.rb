624efc45daaa4ab923b0c46513a3a37437fbcaee
Kevin Old <kevin@kevinold.com>
2010-03-22 17:16:10 -0500

    replace discipleship classes with Sermons promo block

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   current_tab :home
   def index
   @show_sidebar = 1
+  @sermons = Sermon.all :conditions => { :published => 1 }, :order => "date DESC", :limit => 5
   end
 
 end
