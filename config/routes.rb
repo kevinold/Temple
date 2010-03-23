d780454286e99cfffe575af5e784052899d2d51d
Kevin Old <kevin@kevinold.com>
2010-03-23 15:37:46 -0500

    fix routes so that :id can be at root or :section/:id

--- config/routes.rb
+++ config/routes.rb
@@ -76,8 +76,8 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
-  map.page '/:section/:id', :controller => 'pages', :action => 'show'
   map.page '/:id', :controller => 'pages', :action => 'show'
+  map.page '/:section/:id', :controller => 'pages', :action => 'show'
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
