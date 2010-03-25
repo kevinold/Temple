1c02abf2953af50cc9e49db986b16821b6b8d00e
Kevin Old <kevin@kevinold.com>
2010-03-25 00:03:02 -0500

    remove actionmailer_tls so mail works in production on 1.8.7

--- app/models/prayer_mailer.rb
+++ app/models/prayer_mailer.rb
@@ -2,7 +2,7 @@ class PrayerMailer < ActionMailer::Base
   
   def prayer_notification(prayer_handler)
 
-    @recipients  = "prayer@templetoday.com"
+    @recipients  = "kold@templetoday.com"
     @name        = prayer_handler.name
     @from        = prayer_handler.email
     @subject     = "[Prayer/Praise form] #{prayer_handler.subject}"
