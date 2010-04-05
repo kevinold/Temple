cf956556228c619bfe6a75b60f3929529beaac00
Kevin Old <kevin@kevinold.com>
2010-04-05 12:18:51 -0500

    add directory to top nav; open all external links in new tab via appending target=_blank using javscript

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -12,5 +12,11 @@ $(function() {
         $("#services-overlay").animate({top: "-175px"}, 400);
         $(".down").removeClass("up");
   });
+
+  // Open external links in new tab/window
+  $('a').filter(function() {
+      //Compare the anchor tag's host name with location's host name
+      return this.hostname && this.hostname !== location.hostname;
+    }).addClass("external").attr("target", "_blank");
 });
 
