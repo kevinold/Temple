7b45b4895dd635f824fbe1ad3961040d323bd178
Kevin Old <kevin@kevinold.com>
2010-05-25 10:44:10 -0500

    switch to using tracie@templetoday calendar for "upcoming events"

--- lib/gcal.rb
+++ lib/gcal.rb
@@ -5,7 +5,8 @@ require 'pp'
 class Gcal
     def get
         events = Array.new
-        reader = Nokogiri::XML(open("http://www.google.com/calendar/feeds/m2mhi9aoueuujgalci739t8iqc%40group.calendar.google.com/private-b6e1fd08128e7120611022cad150e6d1/full?singleevents=true&orderby=starttime&start-min=#{DateTime.now}&start-max=#{DateTime.now + 30}"))
+#        reader = Nokogiri::XML(open("http://www.google.com/calendar/feeds/m2mhi9aoueuujgalci739t8iqc%40group.calendar.google.com/private-b6e1fd08128e7120611022cad150e6d1/full?singleevents=true&orderby=starttime&start-min=#{DateTime.now}&start-max=#{DateTime.now + 30}"))
+        reader = Nokogiri::XML(open("https://www.google.com/calendar/feeds/tracie%40templetoday.com/private-9a89d99aa461cce0fe823b163bf7f886/full?singleevents=true&orderby=starttime&start-min=#{DateTime.now}&start-max=#{DateTime.now + 30}"))
 
         reader.remove_namespaces!
 
