70e691d58df9309458924bbad8fd757e5da45158
Kevin Old <kevin@kevinold.com>
2010-06-08 12:18:12 -0500

    install Radiant 0.9.0_rc2 linked to bleeding edge git repo via vendor/radiant symlink.  TODO: run "rake db:bootstrap" conflicting tables (user, etc.)

--- config/environment.rb
+++ config/environment.rb
@@ -1,70 +1,85 @@
 # Be sure to restart your server when you modify this file
 
-# Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+# Uncomment below to force Rails into production mode when
+# you don't control web/app server and can't set it the proper way
+# ENV['RAILS_ENV'] ||= 'production'
 
-# Bootstrap the Rails environment, frameworks, and default configuration
+# Specifies gem version of Rails to use when vendor/rails is not present
 require File.join(File.dirname(__FILE__), 'boot')
 
-# Fixes warning for this issue (https://rails.lighthouseapp.com/projects/8994/tickets/4026)
-# until rails 2.3.6 is released
-if Gem::VERSION >= "1.3.6"
-    module Rails
-        class GemDependency
-            def requirement
-                r = super
-                (r == Gem::Requirement.default) ? nil : r
-            end
-        end
-    end
-end
+require 'radius'
 
-Rails::Initializer.run do |config|
-  # Settings in config/environments/* take precedence over those specified here.
-  # Application configuration should go into files in config/initializers
-  # -- all .rb files in that directory are automatically loaded.
+Radiant::Initializer.run do |config|
+  # Skip frameworks you're not going to use (only works if using vendor/rails).
+  # To use Rails without a database, you must remove the Active Record framework
+  config.frameworks -= [ :action_mailer ]
 
-  # Add additional load paths for your own custom dirs
-  # config.load_paths += %W( #{RAILS_ROOT}/extras )
+  # Only load the extensions named here, in the order given. By default all
+  # extensions in vendor/extensions are loaded, in alphabetical order. :all
+  # can be used as a placeholder for all extensions not explicitly named.
+  # config.extensions = [ :all ]
+  
+  # By default, only English translations are loaded. Remove any of these from
+  # the list below if you'd like to provide any of the supported languages
+  config.extensions -= [:dutch_language_pack, :french_language_pack, :german_language_pack,
+                        :italian_language_pack, :japanese_language_pack, :russian_language_pack]
 
-  # Specify gems that this application depends on and have them installed with rake gems:install
-  # config.gem "bj"
-  # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-  # config.gem "sqlite3-ruby", :lib => "sqlite3"
-  # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem 'haml'
-  config.gem 'searchlogic'
-  config.gem 'will_paginate'
-  config.gem 'vestal_versions'
-  config.gem 'hpricot'
+  # Your secret key for verifying cookie session data integrity.
+  # If you change this key, all old sessions will become invalid!
+  # Make sure the secret is at least 30 characters and all random,
+  # no regular words or you'll be exposed to dictionary attacks.
+  config.action_controller.session = {
+    :session_key => '_temple_rails_session',
+    :secret      => '4159895a62f7d9d35ed12ca46d3ae3ce167f621e'
+  }
 
-  # Only load the plugins named here, in the order given (default is alphabetical).
-  # :all can be used as a placeholder for all plugins not explicitly named
-  # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
+  # Comment out this line if you want to turn off all caching, or
+  # add options to modify the behavior. In the majority of deployment
+  # scenarios it is desirable to leave Radiant's cache enabled and in
+  # the default configuration.
+  #
+  # Additional options:
+  #  :use_x_sendfile => true
+  #    Turns on X-Sendfile support for Apache with mod_xsendfile or lighttpd.
+  #  :use_x_accel_redirect => '/some/virtual/path'
+  #    Turns on X-Accel-Redirect support for nginx. You have to provide
+  #    a path that corresponds to a virtual location in your webserver
+  #    configuration.
+  #  :entitystore => "radiant:tmp/cache/entity"
+  #    Sets the entity store type (preceding the colon) and storage
+  #   location (following the colon, relative to Rails.root).
+  #    We recommend you use radiant: since this will enable manual expiration.
+  #  :metastore => "radiant:tmp/cache/meta"
+  #    Sets the meta store type and storage location.  We recommend you use
+  #    radiant: since this will enable manual expiration and acceleration headers.
+  config.middleware.use ::Radiant::Cache
 
-  # Skip frameworks you're not going to use. To use Rails without a database,
-  # you must remove the Active Record framework.
-  # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
+  # Use the database for sessions instead of the cookie-based default,
+  # which shouldn't be used to store highly confidential information
+  # (create the session table with 'rake db:sessions:create')
+  config.action_controller.session_store = :cookie_store
 
   # Activate observers that should always be running
-  # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
+  config.active_record.observers = :user_action_observer
 
-  # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-  # Run "rake -D time" for a list of tasks for finding time zone names.
+  # Make Active Record use UTC-base instead of local time
   config.time_zone = 'UTC'
 
-  # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-  # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
-  # config.i18n.default_locale = :de
-  
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :enable_starttls_auto => true,
-    :address => 'smtp.gmail.com',
-    :port => 587,
-    :authentication => :plain,
-    :user_name => 'noreply@templetoday.com',
-    :password => 'NoPassword316'
-  }
+  # Set the default field error proc
+  config.action_view.field_error_proc = Proc.new do |html, instance|
+    if html !~ /label/
+      %{<span class="error-with-field">#{html} <span class="error">&bull; #{[instance.error_message].flatten.first}</span></span>}
+    else
+      html
+    end
+  end
+
+  config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
+
+  config.after_initialize do
+    # Add new inflection rules using the following format:
+    ActiveSupport::Inflector.inflections do |inflect|
+      inflect.uncountable 'config'
+    end
+  end
 end
