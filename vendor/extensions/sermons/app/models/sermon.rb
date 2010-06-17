c6d1ac48f14cb527c743840fad69d9d3c7e9c155
Kevin Old <kevin@kevinold.com>
2010-06-16 23:13:30 -0500

    fix routes and get sermons rendering to public site; add share_layouts radiant extension

--- vendor/extensions/sermons/app/models/sermon.rb
+++ vendor/extensions/sermons/app/models/sermon.rb
@@ -2,6 +2,10 @@ class Sermon < ActiveRecord::Base
   validates_presence_of :title
   validates_uniqueness_of :title
 
+  def to_param
+    [id, title].join('-').strip.gsub(/[^a-z0-9]+/i, '-').downcase
+  end
+
   def display_date
     date.strftime('%m-%d-%y')
   end
--- vendor/extensions/sermons/config/routes.rb
+++ vendor/extensions/sermons/config/routes.rb
@@ -2,4 +2,5 @@ ActionController::Routing::Routes.draw do |map|
   map.namespace :admin, :member => { :remove => :get } do |admin|
     admin.resources :sermons
   end
+  map.resources :sermons
 end
