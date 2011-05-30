01785f8d0a8798f4933862f0492ee6bf6faa941f
Kevin Old <kevin@kevinold.com>
2011-05-30 15:04:18 -0500

    reconfigure and regenerate Rails 3 app; not bootable yet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,24 +1,2 @@
-# Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-
-  def next_sunday
-    date = Date.today
-    date = date +=1 until date.wday == 0
-    "#{Date::MONTHNAMES[date.mon]} #{date.day}, #{date.year}"
-  end
-
-# Return a title on a per-page basis.
-  def title
-    base_title = "Temple Baptist Church, White House, TN"
-    if @title.nil?
-      base_title
-    else
-      "#{base_title} | #{@title}"
-    end
-  end
-
-  def upcoming_events
-      e = Gcal.new.get
-  end
-
 end
