1f3ce8123fce86bd2232fd5e575dc68459e950d8
Kevin Old <kevin@kevinold.com>
2011-07-29 09:20:50 -0500

    Update announcements sidebar to correctly render future events

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,9 @@ class HomeController < ApplicationController
   def index
     @show_sidebar = 1
     @sermons = Sermon.all :conditions => { :published => 1 }, :order => "date DESC", :limit => 5
-    @announcements = Announcement.all :conditions => { :published => 1 }, :order => "ann_date ASC, ann_time desc", :limit => 10
+    @announcements = Announcement.where("published = 1 and ann_date >= NOW()")
+                                 .order("ann_date ASC, ann_time desc")
+                                 .limit(10)
   end
 
 end
