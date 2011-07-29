6e255dd489f24bce1fbe0275d7922a8d080b11a9
Kevin Old <kevin@kevinold.com>
2011-07-29 15:55:11 -0500

    modifications to deploy.rb; removal of project .bundle/config since it should live in home dir

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,32 +1,35 @@
 require "bundler/capistrano"
+#set :sudo_prompt, "eYWCFgt1"
+#set :bundle_cmd, "sudo bundle"
+default_run_options[:pty] = true
 
-$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
-require "rvm/capistrano"                  # Load RVM's capistrano plugin.
-set :rvm_bin_path, '/usr/local/rvm/bin'
-#set :rvm_ruby_string, 'ree'        # Or whatever env you want it to run in.
+#$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+#require "rvm/capistrano"                  # Load RVM's capistrano plugin.
+#set :rvm_bin_path, '/home/www/.rvm/bin'
+#set :rvm_ruby_string, 'ruby-1.9.2-p180@templetoday.com'        # Or whatever env you want it to run in.
 
-set :application, "temple"
+set :application, "templetoday"
  
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
 set :scm, :git
-set :repository, "git@temple.kevinold.com:#{application}_rails.git"
-set :branch, "master"
+#set :repository, "git@temple.kevinold.com:#{application}_rails.git"
+set :repository, "git@github.com:kevinold/Temple.git"
+set :branch, "feature/rails3_upgrade"
 #set :deploy_via, :remote_cache
-default_run_options[:pty] = true
  
-set :user, 'kevin'
+set :user, 'www'
 set :use_sudo, false
 set :ssh_options, { :forward_agent => true }
  
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-set :deploy_to, "/home/#{user}/#{application}"
+set :deploy_to, "/home/#{user}/sites/#{application}.com"
  
-role :app, "temple.kevinold.com"
-role :web, "temple.kevinold.com"
-role :db,  "temple.kevinold.com", :primary => true
+role :app, "96.126.123.82"
+role :web, "96.126.123.82"
+role :db,  "96.126.123.82", :primary => true
 
 task :prime_cache, :roles => :app do
     run <<-CMD
@@ -34,7 +37,7 @@ task :prime_cache, :roles => :app do
     CMD
 end
 
-after "deploy", "deploy:cleanup", :prime_cache
+after "deploy", "deploy:cleanup" #, :prime_cache
 
 namespace :deploy do
   desc "Restarting mod_rails with restart.txt"
