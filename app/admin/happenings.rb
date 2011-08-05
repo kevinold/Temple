b4a3775a7baecee77861272e166f79d9bd177613
Kevin Old <kevin@kevinold.com>
2011-08-05 16:05:27 -0500

    WIP - update happenings with partial to render image_url if there is one

--- app/admin/happenings.rb
+++ app/admin/happenings.rb
@@ -1,10 +1,11 @@
 ActiveAdmin.register Happening do
-  form :html => { :multipart => true, :enctype => "multipart/form-data" } do |f|
-    f.inputs "Happening", do
-      f.input :name
-      f.input :image, :as => :file
-      f.input :details
-    end    
-    f.buttons
-  end  
+  form :partial => "form"
+  #form :html => { :multipart => true, :enctype => "multipart/form-data" } do |f|
+    #f.inputs "Happening", do
+      #f.input :name
+      #f.input :image, :as => :file
+      #f.input :details
+    #end    
+    #f.buttons
+  #end  
 end
