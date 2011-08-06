df3e7c5ad17a0f2e35acedd4ba64a4a502f31813
Kevin Old <kevin@kevinold.com>
2011-08-05 23:48:12 -0500

    Clean up parital for Happening form

--- app/admin/happenings.rb
+++ app/admin/happenings.rb
@@ -1,11 +1,3 @@
 ActiveAdmin.register Happening do
   form :partial => "form"
-  #form :html => { :multipart => true, :enctype => "multipart/form-data" } do |f|
-    #f.inputs "Happening", do
-      #f.input :name
-      #f.input :image, :as => :file
-      #f.input :details
-    #end    
-    #f.buttons
-  #end  
 end
