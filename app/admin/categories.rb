4efdce351526f7fe835797d9e2887c379f470606
Kevin Old <kevin@kevinold.com>
2011-11-03 10:14:11 -0500

    restrict categories

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,3 +1,5 @@
 ActiveAdmin.register Category do
+  # Add conditional menu support from pull request 177 in github
+  menu :label => "Categories", :if => proc { current_admin_user.id === 2 }
   
 end
