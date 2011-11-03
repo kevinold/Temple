a16ed671cb21ee517333c3f5e7c8ddb4da69c8a3
Kevin Old <kevin@kevinold.com>
2011-11-03 10:28:36 -0500

    fix announcement queries and styling; add sermon notice

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,6 +1,6 @@
 class AnnouncementsController < ApplicationController
   def index
-    @announcements = Announcement.where("published = 1 and ann_date >= curdate() and ann_time >= curtime()")
+    @announcements = Announcement.where("published = 1 and ann_date >= curdate()")
                                  .order("ann_date ASC, ann_time desc")
                                  .limit(50)
   end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
     @next_wednesday = view_context.next_wednesday.to_s
     @show_sidebar = 1
     @sermons = Sermon.all :conditions => { :published => 1 }, :order => "date DESC", :limit => 5
-    @announcements = Announcement.where("published = 1 and ann_date >= curdate() and ann_time >= curtime()")
+    @announcements = Announcement.where("published = 1 and ann_date >= curdate()")
                                  .order("ann_date ASC, ann_time desc")
                                  .limit(10)
     @wed_night_meal = WedNightMeal.where("date <= ?", @next_wednesday).order("date desc").first
